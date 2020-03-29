e67aca5d4fb4b4315406ef010497145be5bb940b
Barış Güzellik <barisguzellik@gmail.com>
2020-03-30 00:17:00 +0300

    Final

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
@@ -7,16 +8,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final wordPair=WordPair.random();
+    final TextStyle _font=const TextStyle(fontSize:30);
     return MaterialApp(
       title: 'First Flutter App',
       home:Scaffold(
         appBar: AppBar(
-          title: Text('First Flutter App'),
+          title: Text(wordPair.asPascalCase),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: Column(
+            mainAxisAlignment:MainAxisAlignment.center,
+            crossAxisAlignment:CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                wordPair.asPascalCase,
+                style: _font
+                ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                  hintText:"Enter word!",
+                  ) ,
+                ),
+              ),
+              RaisedButton(
+                onPressed: (){},
+                child: Text("OK!",style: TextStyle(fontSize:20),),
+              )
+            ],
+          ),
+            
         ),
       ),
     );
   }
+}
+
+void _clickedButton(){
+
 }
